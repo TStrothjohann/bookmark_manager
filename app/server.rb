b1ea5541e7f81bd79c41d1711b2098436c8b927d
Thomas Strothjohann <thomas@strothjohann.net>
2015-02-25 19:04:50 +0000

    Got some wine. And made a flash message appear in case the user confirms his password wrong. Have fun Seb.

--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'data_mapper'
+require './helpers'
+require 'rack-flash'
 
 
 env = ENV['RACK_ENV'] || 'development'
@@ -16,6 +18,7 @@ DataMapper.auto_upgrade!
 
 enable :sessions
 set :session_secret, 'super secret'
+use Rack::Flash
 
 get '/' do
   @links = Link.all
@@ -38,25 +41,22 @@ get '/tags/:text' do
   erb :index
 end
 
-get '/users/new' do
-  erb :"users/new"
-end
-
-
 post '/users' do
-  user = User.create(:email => params[:email],
+  
+    @user = User.create(:email => params[:email],
               :password => params[:password],
               :password_confirmation => params[:password_confirmation])
-  session[:user_id] = user.id
+ if @user.save
+  session[:user_id] = @user.id
   redirect to('/')
-end
-
-helpers do
-
-  def current_user
-    @current_user ||=User.get(session[:user_id]) if session[:user_id]
+  else
+    flash[:notice] = "Sorry, your passwords don't match"
+    erb :"users/new"
   end
-
 end
 
+get '/users/new' do
+  @user = User.new
+  erb :"users/new"
+end
 
