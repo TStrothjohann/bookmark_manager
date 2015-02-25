064582b6b658f6c7291f7b5f1ef4ef4a7ec0b5ad
Thomas Strothjohann <thomas@strothjohann.net>
2015-02-25 15:47:35 +0000

    Wrote you a failing test. We need a tag-input field in our index.erb file now. Go for it.

--- app/server.rb
+++ app/server.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
 require 'data_mapper'
 
+
 env = ENV['RACK_ENV'] || 'development'
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './app/lib/link'
+require './app/lib/tag'
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
