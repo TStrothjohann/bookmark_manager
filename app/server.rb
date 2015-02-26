3936614019f040eb7321daedf730315218a717fa
Thomas Strothjohann <thomas@strothjohann.net>
2015-02-26 18:02:53 +0000

    Added a great black header and started partializing the layout.

--- app/server.rb
+++ app/server.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/partial'
 require 'data_mapper'
-require './helpers'
+require './app/helpers/application'
 require 'rack-flash'
 require './app/models/link'
 require './app/models/tag'
@@ -13,9 +13,11 @@ require_relative 'controllers/tags'
 require_relative 'controllers/users'
 require_relative 'controllers/sessions'
 
+set :public_folder, Proc.new { File.join(root, "..", "public") }
 
 enable :sessions
 set :session_secret, 'super secret'
 use Rack::Flash
 use Rack::MethodOverride
+
 set :partial_template_engine, :erb
